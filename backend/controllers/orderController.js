f8704db8640ad2f9c949cbd10fd915a62121464b
CharanV <charanvoleti55@gmail.com>
2025-08-02 11:42:42 +0530

    changed frontend link

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe=new Stripe(process.env.STRIPE_SECERT_KEY)
 
 // placing user order for frontend
 const placeOrder=async(req,res)=>{
-    const frontend_url="http://localhost:5174";
+    const frontend_url="https://food-ordering-app-rose-pi.vercel.app/";
     try {
         const newOrder=new orderModel({
             userId:req.userId,
