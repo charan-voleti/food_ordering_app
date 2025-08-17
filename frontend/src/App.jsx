cfaa3d4d1f22751bb292ea037e6a27d303a55e6a
CharanV <charanvoleti55@gmail.com>
2025-08-17 22:25:02 +0530

    line2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,7 +21,6 @@ const App = () => {
           <Route path='/' element={<Home />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='/order' element={<PlaceOrder />} />
-          <Route path='/verify' element={<Verify />} />
           <Route path='/myorders' element={<MyOrders />} />
           <Route path="/search" element={<SearchResults />} />
         </Routes>
